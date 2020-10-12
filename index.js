e506db670d61dccf8b807edc9495930214130c25
Sathyam Vellal <sathyam.vellal@gmail.com>
2020-10-12 13:22:59 -0700

    Switch to using chokidar for watching

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-const { Inotify } = require('inotify');
+const chokidar = require('chokidar');
 
 const winston = require('winston');
 const format = require('string-format');
@@ -9,10 +9,10 @@ const { localPath, remotePath } = require('./config');
 
 const myFormat = winston.format.printf(({ level, message, timestamp }) => {
     return '{} ({}) {}'.format(timestamp, level, message);
-  });
+});
 
 const logger = winston.createLogger({
-    level: 'info',
+    level: 'verbose',
     format: winston.format.combine(
         winston.format.timestamp(),
         myFormat
@@ -26,48 +26,34 @@ logger.verbose('Initializing...');
 logger.info('localPath: {}'.format(localPath));
 logger.info('remotePath: {}'.format(remotePath));
 
-var inotify = new Inotify();
+// Initialize watcher.
+var watcher = {};
 
-var callback = function(event) {
+var callback = function(filename) {
     // ignore
-    if (event.name.substring(0, 2) == '.~') {
-        logger.verbose('ignoring change in ' + event.name);
+    if (filename.substring(0, 2) == '.~') {
+        logger.verbose('ignoring change in ' + filename);
         return;
     }
 
-    var mask = event.mask;
-    var type = mask & Inotify.IN_ISDIR ? 'directory ' : 'file ';
-    if (event.name) {
-        type += ' ' + event.name + ' ';
-    } else {
-        type += ' ';
-    }
-    if (mask & Inotify.IN_CLOSE_WRITE) {
-        logger.info(type + 'was modified');
-        logger.verbose('Pushing changes...');
-        exec('rclone -v copy {} {}'.format(localPath, remotePath), (err, stdout, stderr) => {
-            if (err) {
-                logger.error('ERROR: {}'.format(err));
-                process.exit(1);
-            }
-            if (stderr) { logger.http(stderr.trim()); }
-            if (stdout) { logger.http(stderr.trim()); }
-            logger.verbose('Pushed changes... done!');
-            logger.info('Synced');
-        });
-    }
-};
-
-var arg = {
-    path: localPath,
-    watch_for: Inotify.IN_CLOSE_WRITE,
-    callback: callback
+    logger.info(filename + ' was modified');
+    logger.verbose('Pushing changes...');
+    exec('rclone -v copy "{}" "{}"'.format(localPath, remotePath), (err, stdout, stderr) => {
+        if (err) {
+            logger.error('ERROR: {}'.format(err));
+            process.exit(1);
+        }
+        if (stderr) { logger.http(stderr.trim()); }
+        if (stdout) { logger.http(stderr.trim()); }
+        logger.verbose('Pushed changes... done!');
+        logger.info('Synced');
+    });
 };
 
 logger.verbose('Initializing... done!');
 
 logger.verbose('Pulling latest changes...');
-exec('rclone -v copy {} {}'.format(remotePath, localPath), (err, stdout, stderr) => {
+exec('rclone -v copy "{}" "{}"'.format(remotePath, localPath), (err, stdout, stderr) => {
     if (err) {
         logger.error('ERROR: {}'.format(err));
         process.exit(1);
@@ -77,7 +63,11 @@ exec('rclone -v copy {} {}'.format(remotePath, localPath), (err, stdout, stderr)
     logger.verbose('Pulling latest changes... done!');
     logger.info('Synced');
     logger.verbose('Starting watch...');
-    inotify.addWatch(arg);
+    watcher = chokidar.watch(localPath, {
+        ignored: /(^|[\/\\])\../, // ignore dotfiles
+        persistent: true
+    });
+    watcher.on('change', callback);
     logger.verbose('Starting watch... done!');
     logger.info('Watching...');
 });
